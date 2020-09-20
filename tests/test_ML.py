d856bdb16ce3145b6ba88107402d8af7e5f2714b
Javier Bustillo <javier.bustillo@upr.edu>
2020-09-20 19:57:31 -0400

    Fixed test

--- tests/test_ML.py
+++ tests/test_ML.py
@@ -10,12 +10,10 @@ class MLTestCase(unittest.TestCase):
         ml = ML()
         ml.train_model("test_music", "test_music_2", path_to_save="tester_model")
         assert "tester_model" in os.listdir()
-        os.remove("tester_model.arff")
-        os.remove("tester_modelMEANS")
 
     def test_classify(self):
         ml = ML()
-        ml.classify("test_music/514q3otlT6HczfChuLDUSa.mp3", "tester_model")
+        ml.classify("test_music/514q3otlT6HczfChuLDUSa.mp3", "tester_model", 'test_music')
 
 
 if __name__ == '__main__':
