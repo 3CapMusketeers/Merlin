ad772d0d91bce402497a2424b290f413e2ba6bc5
Javier Bustillo <javier.bustillo@upr.edu>
2020-09-21 20:29:14 -0400

    Multiprocessing for getting mp3s

--- SpotifyAPI.py
+++ SpotifyAPI.py
@@ -19,6 +19,7 @@ class SpotifyAPI:
         return json.loads(response.text) if response.text else None
 
     def get_mp3(self, url):
+        print(f"Getting {url}...")
         return requests.get(url).content
 
     def set_token(self, token):
--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -11,7 +11,6 @@ class ModelHandlerTestCase(unittest.TestCase):
         os.environ['SPOT_URL'] = 'https://5f47d89495646700168da0a0.mockapi.io/'
 
     def test_create_model(self):
-        os.chdir("../")
         tracks_dict = [
             {"url": "https://p.scdn.co/mp3-preview/104ad0ea32356b9f3b2e95a8610f504c90b0026b?cid=774b29d4f13844c495f206cafdad9c86",
              "id": "4VqPOruhp5EdPBeR92t6lQ"},
@@ -19,17 +18,18 @@ class ModelHandlerTestCase(unittest.TestCase):
              "id": "2takcwOaAZWiXQijPHIx7B"}
         ]
 
-        ModelHandler().create_model(os.getcwd() + "/tests/1", tracks_dict)
-        assert "1" in os.listdir(os.getcwd() + "/tests")
-        assert "liked" in os.listdir(os.getcwd() + "/tests/1")
-        assert "model" in os.listdir(os.getcwd() + "/tests/1")
-        shutil.rmtree(os.getcwd() + "/tests/1")
+        ModelHandler().create_model("1", tracks_dict)
+        assert "1" in os.listdir()
+        assert "liked" in os.listdir(os.getcwd() + "/1")
+        assert "model" in os.listdir(os.getcwd() + "/1")
+        shutil.rmtree(os.getcwd() + "/1")
 
     def test_classify_tracks(self):
-        os.chdir("../")
         training_tracks = [
-            {'url': "https://p.scdn.co/mp3-preview/104ad0ea32356b9f3b2e95a8610f504c90b0026b?cid=774b29d4f13844c495f206cafdad9c86"},
-            {'url': "https://p.scdn.co/mp3-preview/b326e03624cb098d8387e17aa46669edac0d025a?cid=774b29d4f13844c495f206cafdad9c86"}
+            {'url': "https://p.scdn.co/mp3-preview/104ad0ea32356b9f3b2e95a8610f504c90b0026b?cid=774b29d4f13844c495f206cafdad9c86",
+             "id": "lsdafk"},
+            {'url': "https://p.scdn.co/mp3-preview/b326e03624cb098d8387e17aa46669edac0d025a?cid=774b29d4f13844c495f206cafdad9c86",
+             "id": "dasflkb"}
         ]
         tracks_to_classify = [
             {'url': "https://p.scdn.co/mp3-preview/b326e03624cb098d8387e17aa46669edac0d025a?cid=774b29d4f13844c495f206cafdad9c86",
