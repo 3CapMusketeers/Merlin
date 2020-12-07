e03b88119daba4377b749821cdd48c27be2d3721
Luis Liz <luis.liz@upr.edu>
2020-12-07 19:42:46 -0400

    Deployment file running pure python

--- app.py
+++ app.py
@@ -47,4 +47,4 @@ def check_model(user_id):
 
 
 if __name__ == '__main__':
-    app.run(port=5005)
+    app.run(host='0.0.0.0',port=5005)
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-#echo "Waiting for postgres..."
-#while ! nc -z postgres 5432; do
-#  sleep 0.1
-#done
-#
-#echo "PostgreSQL started"
-
-gunicorn --config gunicorn_config.py app:app
+#gunicorn --config gunicorn_config.py app:app
+export FLASK_ENV=development
+source env/bin/activate 
+python app.py
